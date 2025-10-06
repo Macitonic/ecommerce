f42533be88f47130f837fd640c382919169ccd76
Macitonic <bice4939@gmail.com>
2025-10-07 01:08:17 +0300

    contact-main

---
+++ includes/footer.php
@@ -0,0 +1,84 @@
+<section class="contact" id="contact">
+        <div class="contact-sec1">
+          <h2>E-commerce254</h2>
+          <p>
+            Your one-stop shop for quality home <br> and tech products at unbeatable <br>
+            prices. We’re committed to providing <br> excellent customer service and
+            fast <br>delivery.
+          </p>
+          <div class="contact-icons">
+            <img
+              width="24"
+              height="24"
+              src="https://img.icons8.com/ios-filled/50/FAB005/twitterx--v2.png"
+              alt="twitterx--v2"
+            />
+            <img
+              width="24"
+              height="24"
+              src="https://img.icons8.com/ios-filled/50/FAB005/facebook-new.png"
+              alt="facebook-new"
+            />
+            <img
+              width="24"
+              height="24"
+              src="https://img.icons8.com/ink/48/FAB005/instagram-new.png"
+              alt="instagram-new"
+            />
+            <img
+              width="24"
+              height="24"
+              src="https://img.icons8.com/ink/48/FAB005/whatsapp.png"
+              alt="whatsapp"
+            />
+          </div>
+        </div>
+        <div class="contact-sec2">
+          <h2>Shop</h2>
+          <p>Home Appliances</p>
+          <p>Kitchen & Dining</p>
+          <p>Electronics</p>
+          <p>Lifestyle</p>
+        </div>
+
+        <div class="contact-sec3">
+          <h2>Customer Service</h2>
+          <p>Contact Us</p>
+          <p>FAQ</p>
+          <p>Shipping & Returns</p>
+          <p>Terms & Conditions</p>
+          <p>Privacy Policy</p>
+        </div>
+
+        <div class="contact-sec4">
+          <h2>Contact Us:</h2>
+          <p>
+            <span
+              ><img
+                width="20"
+                height="20"
+                src="https://img.icons8.com/ios-filled/50/FAB005/marker.png"
+                alt="marker"
+            /></span>
+            Njambi street, Ongata Rongai
+          </p>
+          <p>
+            <span
+              ><img
+                width="20"
+                height="20"
+                src="https://img.icons8.com/sf-ultralight-filled/25/FAB005/phone.png"
+                alt="phone" /></span
+            >+254 745407137
+          </p>
+          <p>
+            <span
+              ><img
+                width="20"
+                height="20"
+                src="https://img.icons8.com/ios-filled/50/FAB005/mail.png"
+                alt="mail" /></span
+            >support@ecommerce254.com
+          </p>
+        </div>
+      </section>
---
+++ includes/navbar.php
@@ -0,0 +1,55 @@
+<header>
+  <nav>
+    <!--==Hamburger Icon==-->
+    <div class="nav-top">
+      <div id="menu-toggle" class="menu">
+        <img
+          src="https://img.icons8.com/ios-filled/50/ffffff/menu--v1.png"
+          width="28"
+          height="28"
+          alt="menu"
+        />
+      </div>
+
+      <!--Logo-->
+      <div class="logo">
+        <a href="#"><img src="assets/e-commerce254.png" alt="logo" /></a>
+      </div>
+
+      <!--Drop-down Menu-->
+      <div id="menu">
+        <ul>
+          <li><a href="#home">Home</a></li>
+          <li><a href="#products">Products</a></li>
+          <li><a href="#about">About</a></li>
+          <li><a href="#contact">Contact</a></li>
+        </ul>
+      </div>
+
+      <!--Right: Icons-->
+      <div class="icons">
+        <a href=""
+          ><img
+            width="24"
+            height="24"
+            src="https://img.icons8.com/material-two-tone/24/f39d00/search.png"
+            alt="search"
+        /></a>
+        <a href=""
+          ><img
+            width="24"
+            height="24"
+            src="https://img.icons8.com/material-rounded/24/f39d00/shopping-cart.png"
+            alt="shopping-cart"
+        /></a>
+        <a href=""
+          ><img
+            width="24"
+            height="24"
+            src="https://img.icons8.com/forma-thin-filled/24/f39d00/guest-male.png"
+            alt="guest-male"
+        /></a>
+      </div>
+    </div>
+  </nav>
+</header>
